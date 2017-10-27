c977f7a801e618cc940b6ee724072ffb7bf1d555
jeremy yon <jeremy yon>
2017-10-27 21:19:36 +0100

    third commit

--- GoWebApp.go
+++ GoWebApp.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
     "net/http"
     "text/template"
     "strconv"
@@ -13,32 +12,47 @@ import (
 
 type Message struct {  
     S string
+    GuessedNumber int
+    Status string
 }
 
-func templateHandler(w http.ResponseWriter, r *http.Request){
-    t, _ := template.ParseFiles("template/guess.html")
-    t.Execute(w,Message{S: "Guess a number between 1 and 20"})
 
-     cookies, err := r.Cookie("thing")
+func templateHandler(w http.ResponseWriter, r *http.Request){
+    h2header := "Guess a number between 1 and 20"     
+
+    cookies, err := r.Cookie("thing")
 
      if err == http.ErrNoCookie {
         
         cookies = &http.Cookie{
             Name: "thing",
             Value: strconv.Itoa((rand.Intn(20)+1)),
-            Expires: time.Now().Add(365 * 24 * time.Hour),
-
+            Expires: time.Now().Add(1 * time.Hour),
         }
-        
-    
+    http.SetCookie(w, cookies)
     }
 
-    http.SetCookie(w, cookies)
-
-    io.WriteString(w,cookies.Value)
-
     
 
+    //r.ParseForm()
+    currentNumber,_ := strconv.Atoi(r.FormValue("guess"))
+    //Number{GuessedNumber:r.Form["guess"][0]}
+
+    var status string
+    randNum,_ := strconv.Atoi(cookies.Value)
+    if currentNumber == randNum{
+        status = "You have guessed the correct number. Click New Game for the next random number"
+    } else if currentNumber < randNum{
+        status = "Number is too low"
+    } else if currentNumber > randNum{
+        status = "Number is too high"
+    }
+
+    msg  := &Message{S:h2header, GuessedNumber:currentNumber, Status:status}
+
+    t, _ := template.ParseFiles("template/guess.html")
+    //t.Execute(w,Message{})  
+    t.Execute(w,msg)
 
 }
 
