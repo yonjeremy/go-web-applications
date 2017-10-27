3304a715ce4bfbdfda4528f0f37d0e4301141cac
jeremy yon <jeremy yon>
2017-10-27 22:32:34 +0100

    fourth commit

--- GoWebApp.go
+++ GoWebApp.go
@@ -14,10 +14,14 @@ type Message struct {
     S string
     GuessedNumber int
     Status string
+    GameFinished bool
+    IsNewGame bool
 }
 
 
 func templateHandler(w http.ResponseWriter, r *http.Request){
+    
+    var isNewGame bool
     h2header := "Guess a number between 1 and 20"     
 
     cookies, err := r.Cookie("thing")
@@ -32,7 +36,7 @@ func templateHandler(w http.ResponseWriter, r *http.Request){
     http.SetCookie(w, cookies)
     }
 
-    
+    gameFinished := false 
 
     //r.ParseForm()
     currentNumber,_ := strconv.Atoi(r.FormValue("guess"))
@@ -42,13 +46,21 @@ func templateHandler(w http.ResponseWriter, r *http.Request){
     randNum,_ := strconv.Atoi(cookies.Value)
     if currentNumber == randNum{
         status = "You have guessed the correct number. Click New Game for the next random number"
-    } else if currentNumber < randNum{
+        cookies = &http.Cookie{
+            Name: "thing",
+            Value: strconv.Itoa((rand.Intn(20)+1)),
+            Expires: time.Now().Add(1 * time.Hour),
+        }
+    http.SetCookie(w, cookies)
+    gameFinished = true
+    } else if (currentNumber < randNum) && (currentNumber > 0){
         status = "Number is too low"
     } else if currentNumber > randNum{
         status = "Number is too high"
     }
 
-    msg  := &Message{S:h2header, GuessedNumber:currentNumber, Status:status}
+
+    msg  := &Message{S:h2header, GuessedNumber:currentNumber, Status:status, GameFinished: gameFinished, IsNewGame:isNewGame}
 
     t, _ := template.ParseFiles("template/guess.html")
     //t.Execute(w,Message{})  
@@ -58,7 +70,8 @@ func templateHandler(w http.ResponseWriter, r *http.Request){
 
 
 func main() {
-        http.Handle("/", http.FileServer(http.Dir("./static")))
-        http.HandleFunc("/guess", templateHandler)
-        http.ListenAndServe(":8080", nil)
+    rand.Seed(time.Now().UTC().UnixNano())
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+    http.HandleFunc("/guess", templateHandler)
+    http.ListenAndServe(":8080", nil)
 }
